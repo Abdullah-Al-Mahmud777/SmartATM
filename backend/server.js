f9e18e63a4793180880893caa47a01c27f7f1f33
Abdullah-Al-Mahmud <abdullahalmahmudmahmud777@gmail.com>
2026-02-23 14:58:01 +0600

    update port number for deploy render

--- backend/server.js
+++ backend/server.js
@@ -169,7 +169,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(PORT, () => {
