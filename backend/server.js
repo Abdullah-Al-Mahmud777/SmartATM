0c33dd66e779fccacc50f4e2795f0254ec6dee5a
Abdullah-Al-Mahmud <abdullahalmahmudmahmud777@gmail.com>
2026-02-23 23:37:51 +0600

    connect vercel

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ connectDB();
 
 // CORS Configuration - Allow all origins for testing
 const corsOptions = {
-  origin: ["https://smart-atm-jade.vercel.app", "http://localhost:5173"],
+  origin: ["https://smart-atm-three.vercel.app/atm/home", "http://localhost:5173"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
